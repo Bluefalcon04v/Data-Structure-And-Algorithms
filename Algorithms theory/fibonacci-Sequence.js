7847140dca9d21512756924967b48ecce7157846
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-09-06 09:35:21 +0530

    Add files via upload
    solving errors of fibonacci sequence

--- Algorithms theory/fibonacci-Sequence.js
+++ Algorithms theory/fibonacci-Sequence.js
@@ -3,13 +3,23 @@
 
 function fibonacci(n){
     const fib = [0, 1];
-    for(let i = 2; i < n; i++){
-        fib[i] = fib[i - 1] + fib[i - 2];
+    if(n == 0){
+        return null;
     }
-    return fib
+    else if(n == 1){
+        return 0;
+    }
+    else {
+        for(let i = 2; i < n; i++){
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib;
+    }
 }
 
-console.log(fibonacci(6));
-console.log(fibonacci(12));
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(4));
 
 //  the Big-O complexity is linear ie O(n)
