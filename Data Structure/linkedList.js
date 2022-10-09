54e78ef19bda00803fa112ae7ff7b30d644d94fe
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-10-09 14:03:15 +0530

    Add files via upload
    linked list update

--- Data Structure/linkedList.js
+++ Data Structure/linkedList.js
@@ -1,6 +1,6 @@
 // About Linked List
 
-class node{
+class Node{
     constructor(value){
         this.value = value
         this.next = null
@@ -17,7 +17,24 @@ class LinkedList{
     getSize(){
         return this.size
     }
+    prepend(value){
+        const node = new Node(value)           // making a new Node
+        if(this.isEmpty()){                    // checking if list is Empty new Node will be set as a head node, else it will connect new node at the beginning 
+            this.head = node
+        }else {
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
 }
+
 const list = new LinkedList
 console.log('list is empty?', list.isEmpty());
+
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+list.prepend(40)
+
 console.log('list size is ', list.getSize());
