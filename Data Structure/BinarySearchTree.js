d81fbc3d9c9ac310101e31ebccc420beaf25a106
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-10-24 11:21:18 +0530

    Add files via upload
    Binary Search tree Update

--- Data Structure/BinarySearchTree.js
+++ Data Structure/BinarySearchTree.js
@@ -16,7 +16,61 @@ class BinarySearchTree{
     isEmpty(){
         return this.root === null
     }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left, newNode)
+            }
+        }
+        else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+    search(root, value){
+        if(!root){
+            return false
+        }
+        else{
+            if(root.value === value){
+                return true
+            }
+            else if(value < root.value){
+                return this.search(root.left, value)
+            }
+            else{
+                return this.search(root.right, value)
+            }
+        }
+    }
 }
+
 
 const bst = new BinarySearchTree
 console.log("Tree is Empty? " + bst.isEmpty());
+
+bst.insert(10)
+bst.insert(20)
+bst.insert(5)
+bst.insert(3)
+bst.insert(13)
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 20));
+console.log(bst.search(bst.root, 9));
