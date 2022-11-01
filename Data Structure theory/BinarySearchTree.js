1f0e5ec7ee11e60f977761425c2c0fc30dc77573
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-11-01 18:31:44 +0530

    Add files via upload

---
+++ Data Structure theory/BinarySearchTree.js
@@ -0,0 +1,182 @@
+// Implementing Binary Search Tree
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+
+    isEmpty(){
+        return this.root === null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left, newNode)
+            }
+        }
+        else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+    search(root, value){
+        if(!root){
+            return false
+        }
+        else{
+            if(root.value === value){
+                return true
+            }
+            else if(value < root.value){
+                return this.search(root.left, value)
+            }
+            else{
+                return this.search(root.right, value)
+            }
+        }
+    }
+
+
+    // preOrder, inOrder, postOrder is a part of depth first search 
+
+    preOrder(root){
+        if(root){
+            console.log(root.value);
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left)
+            console.log(root.value);
+            this.inOrder(root.right)
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value);
+        }
+    }
+
+    breadthFirstSearch(){
+        const queue = []
+        queue.push(this.root)
+        while(queue.length){
+            let current = queue.shift()
+            console.log(current.value);
+            if(current.left){
+                queue.push(current.left)
+            }
+            if(current.right){
+                queue.push(current.right)
+            }
+        }
+    }
+
+
+    min(root){
+        if(!root.left){
+            return root.value
+        }
+        else{
+            return this.min(root.left)
+        }
+    }
+
+
+    max(root){
+        if(!root.right){
+            return root.value
+        }
+        else{
+            return this.max(root.right)
+        }
+    }
+
+    delete(value){
+        this.root = this.deleteNode(this.root, value)
+    }
+    deleteNode(root, value){
+        if(root === null){
+            return root
+        }
+        if(value < root.value){
+            root.left = this.deleteNode(root.left, value)
+        }else if(value > root.value){
+            root.right = this.deleteNode(root.right, value)
+        }
+        else{
+            if(!root.left && !root.right){
+                return null
+            }
+            if(!root.left){
+                return root.right
+            }
+            else if(!root.right){
+                return root.left
+            }
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root
+    }
+
+}
+
+
+const bst = new BinarySearchTree
+console.log("Tree is Empty? " + bst.isEmpty());
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+
+// console.log(bst.search(bst.root, 10));
+// console.log(bst.search(bst.root, 20));
+// console.log(bst.search(bst.root, 9));
+
+
+// bst.preOrder(bst.root)
+// bst.inOrder(bst.root)
+// bst.postOrder(bst.root)
+
+
+// bst.breadthFirstSearch()
+
+// console.log(bst.min(bst.root));
+// console.log(bst.max(bst.root));
+
+bst.breadthFirstSearch()
+bst.delete(5)
+bst.breadthFirstSearch()
