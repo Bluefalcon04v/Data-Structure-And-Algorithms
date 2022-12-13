dcd50e591b1dbdb5ba5eaf0f92b33a6fc46317c1
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2022-12-13 19:58:43 +0530

    Add files via upload
    two sum update

--- Problems/Arrays Problems/1st)Two_Sum.js
+++ Problems/Arrays Problems/1st)Two_Sum.js
@@ -19,12 +19,26 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-var twoSum = function(nums, target) {    // Time Complexity O(n2)
-    for(let i= 0; i <= nums.length; i++){
-        for(let j = i+1; j <= nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j]
-            }
-        }
-    }return [-1, -1]
+
+
+ // time complexity O(n^2) 
+ var twoSum = function(nums, target) {
+    // for(let i= 0; i <= nums.length; i++){
+    //     for(let j = i+1; j <= nums.length; j++){
+    //         if(nums[i] + nums[j] === target){
+    //             return [i, j]
+    //         }
+    //     }
+    // }return [-1, -1]
+
+
+// time Complexity O(n)
+    let map = new Map()
+    for(let i =0; i<= nums.length; i++){
+        let num1 = nums[i]
+        let num2 = target - num1
+        if(map.has(num2)){
+            return [i, map.get(num2)]
+        }map.set(num1, i)
+    }
 };
